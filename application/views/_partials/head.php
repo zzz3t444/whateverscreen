978477b5d4d93eabb310e0d85af4d290f527997f
Zeeta:) <126560008+zzz3t444@users.noreply.github.com>
2024-06-03 10:09:22 -0700

    Update

--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -2,5 +2,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= isset($meta['title']) ? $meta['title'] : 'Beritacoding.com' ?></title>
+<link rel="icon" href="https://res.cloudinary.com/ddy7p8yrj/image/upload/v1717434303/Group_1808_rcfhrp.png" type="image/x-icon" />
 
 <link rel="stylesheet" href="<?= base_url('assets/css/main.css') ?>" />
--- application/views/admin/_partials/head.php
+++ application/views/admin/_partials/head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Admin - Beritacoding</title>
+<title>Admin - Whatever Screen</title>
+<link rel="icon" href="https://res.cloudinary.com/ddy7p8yrj/image/upload/v1717434303/Group_1808_rcfhrp.png" type="image/x-icon" />
 
 <link rel="stylesheet" href="<?= base_url('assets/css/admin.css') ?>" />
